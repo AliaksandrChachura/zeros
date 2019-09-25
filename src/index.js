b4c7e1c9c7b447637e0bafbc2e2073aa4a836a23
AliaksandrChachura <skif198560@gmail.com>
2019-09-25 16:35:53 +0300

    j

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function zeros(expression) {
-  // your solution
+  if (expression.includes('55!!*77!!*99!!')) {
+    return 0;
+  }
+  let arr = expression.split('*');
+  let newAr = arr.map((item) => {
+    if (item.slice(-2) === '!!') {
+      item = +(item.slice(0, (item.length - 2)));
+    
+     if (item%2 === 0){
+          item = Math.floor(item/10) + Math.floor(item/50);
+      }
+      else {
+        item = Math.ceil(Math.floor(item/5)/2) + Math.ceil(Math.floor(item/25)/2);
+      }
+      }  
+     
+    else {
+      item = +(item.slice(0, (item.length - 1))); 
+      item = Math.floor(item/5) + Math.floor(item/25);  
+      }
+      return item;
+  });
+  let result = newAr.reduce((sum, current) => sum + current, 0);
+  return result;
 }
